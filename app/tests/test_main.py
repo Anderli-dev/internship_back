46cfe7da0398c3d10f690cdd8920079e2a9a346d
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-02-25 06:49:16 +0200

    added tests

---
+++ app/tests/test_main.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+from main import app
+client = TestClient(app)
+
+
+def test_home_status_code():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_home_response_structure():
+    response = client.get("/")
+    json_data = response.json()
+    assert "status_code" in json_data
+    assert "detail" in json_data
+    assert "result" in json_data
+
+
+def test_home_response_values():
+    response = client.get("/")
+    json_data = response.json()
+    assert json_data["status_code"] == 200
+    assert json_data["detail"] == "ok"
+    assert json_data["result"] == "working"
+
