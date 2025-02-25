7f9d576467bf450e1f0f40490b4091816e38b6e6
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-02-25 08:16:54 +0200

    implemented CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+origins = [
+    "http://localhost:3000",  # For React frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home() -> dict:
