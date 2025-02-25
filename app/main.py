e03c47c6b8eec1564e95a2883dece8f0d8517f8c
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-02-25 06:48:25 +0200

    added auto-restart for comfort using

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 def home() -> dict:
     return {"status_code": 200, "detail": "ok", "result": "working"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
