3ff5187ecf52f03a05dce0bbfbcee64843b5527c
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-05 08:38:43 +0200

    added computed field for redis and psql

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,20 +1,33 @@
+from pydantic import computed_field
 from pydantic_settings import BaseSettings
 import pathlib
 
 class Settings(BaseSettings):
-    app_name: str = "MyApp"
-    debug: bool = False
-    
     app_host: str
     app_port: int
     
-    redis_url: str
-    database_url: str
+    redis_host: str
+    redis_port: int
+    
+    @computed_field
+    @property
+    def redis_url(self) -> str:
+        return (f"redis://{self.redis_host}:{self.redis_port}")
 
     postgres_user: str
     postgres_password: str
     postgres_db: str
-
+    postgres_host: str
+    postgres_port: int
+    
+    @computed_field
+    @property
+    def database_url(self) -> str:
+        return (f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
+                f"{self.postgres_host}:{self.postgres_port}/{self.postgres_db}")
+    
+    
+    
     class Config:
         env_file = str(pathlib.Path(__file__).resolve().parents[2] / ".env")
         
