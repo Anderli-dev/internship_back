25b03a086ecdf0f4734d638ec471ffc8d5beb20d
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-05 07:22:39 +0200

    removed logging from geting app url

--- app/utils/get_app_url.py
+++ app/utils/get_app_url.py
@@ -1,14 +1,11 @@
 import os
 
-from core.settings import logger
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_app_url():
-    logger.info("Getting app url.")
     app_host = os.getenv("APP_HOST")
     app_port = os.getenv("APP_PORT")
     
-    logger.info("Getting app url success.")
     return f"http://{app_host}:{app_port}"
