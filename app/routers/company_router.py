6bf4740facf696f90a903c028dad13a7411abc4a
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-25 09:53:57 +0200

    fixed delete

--- app/routers/company_router.py
+++ app/routers/company_router.py
@@ -1,9 +1,11 @@
 from db.models.user import User
-from db.schemas.company_shema import CompanyCreate, CompanyResponse, CompanyUpdate
+from db.schemas.company_shema import (CompanyCreate, CompanyResponse,
+                                      CompanyUpdate)
 from db.session import get_db
 from fastapi import APIRouter, Depends
 from services.auth import Auth
 from services.company_service.create import CompanyCreateService
+from services.company_service.delete import CompanyDeleteService
 from services.company_service.update import CompanyUpdateService
 from services.user_service.read import UserReadService
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -21,3 +23,9 @@ async def update_company_endpoint(company_id: int, company_data: CompanyUpdate, 
     user: User = await UserReadService().read_auth_user(db, payload=payload)
     
     return await CompanyUpdateService.update_company(db, company_id, company_data, user_id=user.id)
+
+@router.delete("/{company_id}")
+async def delete_company_endpoint(company_id: int, db: AsyncSession = Depends(get_db), payload: dict = Depends(Auth().get_token_payload)):
+    user: User = await UserReadService().read_auth_user(db, payload=payload)
+    
+    return await CompanyDeleteService.delete_company(db, company_id, user_id=user.id)
--- app/services/company_service/delete.py
+++ app/services/company_service/delete.py
@@ -1,8 +1,9 @@
-from db.models.company_user_role import RoleEnum
+from db.models.company_user_role import CompanyUserRole, RoleEnum
 from db.models.company import Company
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.sql.expression import delete
 from utils.decorators.check_user_role import check_user_role
 
 
@@ -10,11 +11,13 @@ class CompanyDeleteService:
     @staticmethod
     @check_user_role(role_enum=RoleEnum.owner)
     async def delete_company(db: AsyncSession, company_id: int, user_id: int):
+        await db.execute(delete(CompanyUserRole).where(CompanyUserRole.company_id == company_id))
+        
         company = await db.execute(select(Company).filter(Company.id == company_id))
         company = company.scalars().first()
         if not company:
             raise HTTPException(status_code=404, detail="Company not found")
 
-        db.delete(company)
-        db.commit()
+        await db.delete(company)
+        await db.commit()
         return {"detail": "Company deleted"}
