5211e1b36e8c88fe47156a732be00ffd732bf993
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-04-14 09:04:46 +0300

    fixed custom exception handlers

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -11,6 +11,6 @@ def token_expired_handler(request: Request, exc: exceptions.ExpiredSignatureErro
     logger.error("Token expired!")
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired!")
 
-def invalid_token_handler(request: Request, exc: exceptions.ExpiredSignatureError):
+def invalid_token_handler(request: Request, exc: InvalidToken):
     logger.error("Invalid token!")
-    raise HTTPException(status_code=401, detail="Invalid token!")
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token!")
